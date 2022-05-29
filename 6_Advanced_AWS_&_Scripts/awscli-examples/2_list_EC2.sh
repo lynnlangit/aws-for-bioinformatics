5e25dff5be88229df53589f5917820528775edca
Lynn Langit <lynnlangit@live.com>
2022-05-28 22:45:38 -0500

    updating scripts

--- 6_Advanced_AWS_&_Scripts/awscli-examples/2_list_EC2.sh
+++ 6_Advanced_AWS_&_Scripts/awscli-examples/2_list_EC2.sh
@@ -1,9 +1,22 @@
-# To run in  Shell, set to your login and project directory, then run:
-aws init --profile=default
+### To run in  Shell, set to your login and project directory, then run:
+`aws init --profile=default`
 
-# List all compute instances in the project
-aws ec2 describe-instances
+### List all compute instances in the project
+`aws ec2 describe-instances`
 
-# Grep the serial console output from all instances in the project
-# for a specific pattern in the output.
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value,State.Name,PublicDnsName,PublicIpAddress,PrivateDnsName,PrivateIpAddress,LaunchTime]' --output text | grep -i "^i-.*running"
+### Grep the serial console output from all instances in the project
+### for a specific pattern in the output.
+`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value,State.Name,PublicDnsName,PublicIpAddress,PrivateDnsName,PrivateIpAddress,LaunchTime]' --output text | grep -i "^i-.*running"`
+
+### List all compute instances in the project
+`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value,State.Name,PublicDnsName,PublicIpAddress,PrivateDnsName,PrivateIpAddress,LaunchTime]' --output text | grep -i "^i-.*running"`
+
+### Stop all compute instances in the project
+`aws ec2 stop-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --output text | grep -i "^i-.*running")`
+
+### Start all compute instances in the project
+`aws ec2 start-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --output text | grep -i "^i-.*stopped")`
+
+### Terminate all compute instances in the project
+`aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --output text | grep -i "^i-.*running")`
+
--- 6_Advanced_AWS_&_Scripts/awscli-examples/3_manage_RDS.sh
+++ 6_Advanced_AWS_&_Scripts/awscli-examples/3_manage_RDS.sh
@@ -1,19 +1,19 @@
-# To run in  Shell, set to your login and project directory, then run:
-aws init --profile=default
+### To run in  Shell, set to your login and project directory, then run:
+`aws init --profile=default`
 
-# Create a RDS instance
-aws rds create-db-instance --db-instance-identifier {your-db-instance-identifier} --db-instance-class db.t2.micro --engine MySQL --master-username {your-username} --master-user-password {your-password} --allocated-storage 5 --db-name {your-db-name} --no-multi-az --no-publicly-accessible --vpc-security-group-ids {your-security-group-id} --db-subnet-group-name {your-subnet-group-name}
+### Create a RDS instance
+`aws rds create-db-instance --db-instance-identifier {your-db-instance-identifier} --db-instance-class db.t2.micro --engine MySQL --master-username {your-username} --master-user-password {your-password} --allocated-storage 5 --db-name {your-db-name} --no-multi-az --no-publicly-accessible --vpc-security-group-ids {your-security-group-id} --db-subnet-group-name {your-subnet-group-name}`
 
-# Assign the instance an IPv4 address
-aws rds modify-db-instance --db-instance-identifier {your-db-instance-identifier} --vpc-security-group-ids {your-security-group-id} --apply-immediately
+### Assign the instance an IPv4 address
+`aws rds modify-db-instance --db-instance-identifier {your-db-instance-identifier} --vpc-security-group-ids {your-security-group-id} --apply-immediately`
 
-# Set the root user password for the instance.  Replace the [PASSWORD]
-# placeholder with the actual password you want to use.
-aws rds modify-db-instance-password --db-instance-identifier {your-db-instance-identifier} --master-user-password [PASSWORD]
+### Set the root user password for the instance.  Replace the [PASSWORD]
+### placeholder with the actual password you want to use.
+`aws rds modify-db-instance-password --db-instance-identifier {your-db-instance-identifier} --master-user-password [PASSWORD]`
 
-# Connect to the instance.
-aws rds connect-to-db --db-instance-identifier {your-db-instance-identifier} --db-user-name {your-username} --db-password [PASSWORD]
+### Connect to the instance.
+`aws rds connect-to-db --db-instance-identifier {your-db-instance-identifier} --db-user-name {your-username} --db-password [PASSWORD]`
 
-# List all RDS instances in the project
-aws rds describe-db-instances
+### List all RDS instances in the project
+`aws rds describe-db-instances`
 
