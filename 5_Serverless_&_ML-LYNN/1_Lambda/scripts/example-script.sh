98caf79c54992155b3cdd14d2ac3b66f1142e320
Lynn Langit <lynnlangit@live.com>
2022-08-28 12:19:33 -0500

    renumber whitepapers

--- 5_Serverless_&_ML-LYNN/1_Lambda/scripts/example-script.sh
+++ 5_Serverless_&_ML-LYNN/1_Lambda/scripts/example-script.sh
@@ -1,1 +1,15 @@
-# example script here
+# Lambda example script
+aws lambda invoke --function-name example-function --payload file://payload.json output.json
+
+# Output:
+aws lambda invoke --function-name example-function --payload file://payload.json output.json
+{
+  "statusCode": 200,
+  "body": "Hello from Lambda!"
+}
+
+# Payload:
+{
+  "name": "John Doe"
+}
+
